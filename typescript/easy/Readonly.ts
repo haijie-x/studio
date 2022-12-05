27934a7473dc6e2774e63cf33ae9716095436bf5
xiehaijie <320974775@qq.com>
2022-12-05 23:15:51 +0800

    feat: 3 easy

---
+++ typescript/easy/Readonly.ts
@@ -0,0 +1,21 @@
+/* _____________ Your Code Here _____________ */
+
+type MyReadonly<T> = {
+  readonly [K in keyof T]: T[K]
+}
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<MyReadonly<Todo1>, Readonly<Todo1>>>,
+]
+
+interface Todo1 {
+  title: string
+  description: string
+  completed: boolean
+  meta: {
+    author: string
+  }
+}
