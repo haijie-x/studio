c862385d44de4cac6cba93b7f196ec1b5a3d83c5
xiehaijie <320974775@qq.com>
2022-12-07 18:00:54 +0800

    feat: 2 easy

---
+++ typescript/easy/Exclude.ts
@@ -0,0 +1,14 @@
+/* _____________ Your Code Here _____________ */
+
+// When condition type is generic type && union type, it will become distributive as well as foreachable
+// 当参数类型是范型同时传入联合类型，计算类型时会进行遍历
+type MyExclude<T, U> = T extends U ? never : T
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<MyExclude<'a' | 'b' | 'c', 'a'>, 'b' | 'c'>>,
+  Expect<Equal<MyExclude<'a' | 'b' | 'c', 'a' | 'b'>, 'c'>>,
+  Expect<Equal<MyExclude<string | number | (() => void), Function>, string | number>>,
+]
