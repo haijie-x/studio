a3c787cc61f32883f6ab18e821aedbcf8604d5f9
xiehaijie <320974775@qq.com>
2022-12-07 13:00:11 +0800

    chore: update eslint & prettier

--- typescript/easy/Readonly.ts
+++ typescript/easy/Readonly.ts
@@ -7,9 +7,7 @@ type MyReadonly<T> = {
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
 
-type cases = [
-  Expect<Equal<MyReadonly<Todo1>, Readonly<Todo1>>>,
-]
+type cases = [Expect<Equal<MyReadonly<Todo1>, Readonly<Todo1>>>]
 
 interface Todo1 {
   title: string
