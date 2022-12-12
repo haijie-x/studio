4a3c78014092fafaf2b9869af94ebe59ca83892c
xiehaijie <320974775@qq.com>
2022-12-12 23:54:53 +0800

    mend

---
+++ react/react-app/src/index.js
@@ -0,0 +1,9 @@
+import ReactDOM from 'react-dom'
+import React from 'react'
+import App from './app.js'
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.querySelector('#root'),
+)
