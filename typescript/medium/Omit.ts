e19b859898d4e5f07cc306630486796e3e0f9dcc
xiehaijie <320974775@qq.com>
2022-12-14 02:13:52 +0800

    ts: 2 medium

---
+++ typescript/medium/Omit.ts
@@ -0,0 +1,31 @@
+/* _____________ Your Code Here _____________ */
+
+type MyOmit<T, K extends keyof T> = {
+  [U in keyof T as U extends K ? never : U]: T[U]
+}
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<Expected1, MyOmit<Todo, 'description'>>>,
+  Expect<Equal<Expected2, MyOmit<Todo, 'description' | 'completed'>>>,
+]
+
+// @ts-expect-error
+type error = MyOmit<Todo, 'description' | 'invalid'>
+
+interface Todo {
+  title: string
+  description: string
+  completed: boolean
+}
+
+interface Expected1 {
+  title: string
+  completed: boolean
+}
+
+interface Expected2 {
+  title: string
+}
