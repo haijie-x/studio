f420504058ae64ffdccc1ebe0babb32f87c87919
xiehaijie <320974775@qq.com>
2022-12-13 13:10:51 +0800

    react: renew

--- react/react-app/src/index.js
+++
@@ -1,9 +0,0 @@
-import ReactDOM from 'react-dom'
-import React from 'react'
-import App from './app.js'
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.querySelector('#root'),
-)
