64704fc454df397784e171d56e33ea44d8237863
xiehaijie <320974775@qq.com>
2022-12-16 01:18:38 +0800

    ts: 2 medium

---
+++ typescript/medium/TupleToUnion.ts
@@ -0,0 +1,11 @@
+/* _____________ Your Code Here _____________ */
+
+type TupleToUnion<T> = T extends Array<infer V> ? V : never
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<TupleToUnion<[123, '456', true]>, 123 | '456' | true>>,
+  Expect<Equal<TupleToUnion<[123]>, 123>>,
+]
---
+++ typescript/test/test.ts
@@ -0,0 +1,4 @@
+const foo: number = 123
+const bar: string = '123'
+
+const arr = [foo, bar] as const
