e29cdd7ce75459d0335c9246fc375f586efc75d0
xiehaijie <320974775@qq.com>
2022-12-16 21:04:15 +0800

    ts: 2 medium

---
+++ typescript/medium/LastofArray.ts
@@ -0,0 +1,10 @@
+/* _____________ Your Code Here _____________ */
+
+type Last<T extends any[]> = T extends [...any[], infer L] ? L : never
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<Last<[3, 2, 1]>, 1>>,
+  Expect<Equal<Last<[() => 123, { a: string }]>, { a: string }>>,
+]
