99526b15615c578796e7eb6ebddd7d60803f9c50
xiehaijie <320974775@qq.com>
2022-12-17 16:26:28 +0800

    react: refactor to react-transition-group

--- react/src/components/CodePreview.tsx
+++ react/src/components/CodePreview.tsx
@@ -1,7 +1,8 @@
 import * as React from 'react'
 import styled from 'styled-components'
 import article from '@/consts/article'
-import { Fade } from './Fade'
+import { SwitchTransition, CSSTransition } from 'react-transition-group'
+import '@/styles/animate.css'
 
 export interface ICodePreviewProps {
   curIdx: number
@@ -11,21 +12,39 @@ const Wrapper = styled.div`
   flex: 1;
   display: flex;
   background-color: rgb(32, 34, 38);
-  color: rgb(198, 221, 217);
-  word-break: break-all;
-  font-size: 30px;
+  .code__wrapper {
+    flex: 1;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: 0px 30px;
+    font-size: 15px;
+    word-break: break-all;
+    white-space: pre;
+    color: rgb(198, 221, 217);
+  }
 `
 
 export function CodePreview(props: ICodePreviewProps) {
+  const nodeRef = React.useRef<HTMLDivElement | null>(null)
+
   return (
     <Wrapper>
-      {article.map((item, index) => {
-        return (
-          <Fade show={index == props.curIdx} key={index}>
-            <div>{item.code}</div>
-          </Fade>
-        )
-      })}
+      <SwitchTransition>
+        <CSSTransition
+          key={props.curIdx}
+          nodeRef={nodeRef}
+          addEndListener={done => {
+            nodeRef.current!.addEventListener('transitionend', done, false)
+          }}
+          classNames="code"
+          appear
+        >
+          <div ref={nodeRef} className="code__wrapper">
+            <span>{article[props.curIdx].code}</span>
+          </div>
+        </CSSTransition>
+      </SwitchTransition>
     </Wrapper>
   )
 }
