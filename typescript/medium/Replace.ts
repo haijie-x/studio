75ad3b64651ea80586986a0a2b4e34c4f6cbec02
xiehaijie <320974775@qq.com>
2023-01-03 23:11:46 +0800

    ts: 2 medium

---
+++ typescript/medium/Replace.ts
@@ -0,0 +1,23 @@
+/* _____________ Your Code Here _____________ */
+
+type Replace<
+  S extends string,
+  From extends string,
+  To extends string,
+> = S extends `${infer Left}${From}${infer Right}`
+  ? From extends ''
+    ? S
+    : `${Left}${To}${Right}`
+  : S
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<Replace<'foobar', 'bar', 'foo'>, 'foofoo'>>,
+  Expect<Equal<Replace<'foobarbar', 'bar', 'foo'>, 'foofoobar'>>,
+  Expect<Equal<Replace<'foobarbar', '', 'foo'>, 'foobarbar'>>,
+  Expect<Equal<Replace<'foobarbar', 'bar', ''>, 'foobar'>>,
+  Expect<Equal<Replace<'foobarbar', 'bra', 'foo'>, 'foobarbar'>>,
+  Expect<Equal<Replace<'', '', ''>, ''>>,
+]
